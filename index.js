9c9d315a188f3c750f9801c0c1b0a768159b2969
Oscar Batlle <oscarbatlle@gmail.com>
2015-06-27 01:15:32 -0400

    Add functionality to setup nicknames

--- index.js
+++ index.js
@@ -2,26 +2,87 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require('express');
-var path = require('path')
+var path = require('path');
 
-app.get('/', function(request, response){
+app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
-
+// Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', function(socket){
-    console.log('User connected');
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
 
-    socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+    // Tigger modal
+    socket.on('connect', function (data) {
+        io.emit('connect', data);
+    });
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
 
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        // remove the username from global usernames list
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
     });
 });
 
-http.listen(8888, function(){
+http.listen(8888, function () {
     console.log('Listening on port : 8888');
 });
