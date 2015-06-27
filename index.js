8e0bbe4b06862cbd78e8b2b03998e636ac825fa9
Oscar Batlle <oscarbatlle@gmail.com>
2015-06-26 21:06:57 -0400

    Add middleware to include the CSS stylesheet
    Since .css files are static files you have to serve them to the clients.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express');
+var path = require('path')
 
 app.get('/', function(request, response){
-    response.sendFile(__dirname + 'index.html');
+    response.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
     console.log('User connected');
