e7f8e5a7b45e69218b40d800d0557f8549fc4d20
Oscar Batlle <oscarbatlle@gmail.com>
2015-06-26 20:20:25 -0400

    Create a new instace of socket.io
    Initialize a new instance of socket.io by passing the http
    (the HTTP server) object and setup the connection event for incoming sockets.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response){
     response.sendfile(__dirname + 'index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('User connected');
 });
 
 http.listen(8888, function(){
